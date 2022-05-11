715ef0dd0466d43d6fc1b045d109696921e4419b
Daniel Vasyanovich <danielvsi12@gmail.com>
2022-05-11 19:40:29 +0300

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router';
+import { Home } from './components/Home';
+import { Profile } from './components/Profile';
+import { Stories } from './components/Stories';
+import { Actions } from './components/Actions';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <html dir="rtl">
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/%D7%A4%D7%A2%D7%95%D7%9C%D7%95%D7%AA" element={<Actions />} />
+        <Route path="/%D7%A4%D7%A8%D7%95%D7%A4%D7%99%D7%9C" element={<Profile />} />
+        <Route path="/%D7%AA%D7%95%D7%9B%D7%9F" element={<Stories />} />
+      </Routes>
+    </html>
     </div>
   );
 }
