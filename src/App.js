ee4bedec5b6ecb897643f0940ab060fbe700f9dc
ivrifreud <ivrifreud@gmail.com>
2022-05-12 02:25:32 +0300

    basic home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import { Routes, Route } from 'react-router';
-import { Home } from './components/Home';
+import { Home } from './components/Pages/Home';
 import { Profile } from './components/Profile';
 import { Stories } from './components/Stories';
 import { Actions } from './components/Actions';
 
 function App() {
-  return (
-    <div className="App">
-    <html dir="rtl">
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/%D7%A4%D7%A2%D7%95%D7%9C%D7%95%D7%AA" element={<Actions />} />
-        <Route path="/%D7%A4%D7%A8%D7%95%D7%A4%D7%99%D7%9C" element={<Profile />} />
-        <Route path="/%D7%AA%D7%95%D7%9B%D7%9F" element={<Stories />} />
-      </Routes>
-    </html>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        html dir = "rtl" >
+        <
+        NavBar / >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/%D7%A4%D7%A2%D7%95%D7%9C%D7%95%D7%AA"
+        element = { < Actions / > }
+        /> <
+        Route path = "/%D7%A4%D7%A8%D7%95%D7%A4%D7%99%D7%9C"
+        element = { < Profile / > }
+        /> <
+        Route path = "/%D7%AA%D7%95%D7%9B%D7%9F"
+        element = { < Stories / > }
+        /> <
+        /Routes> <
+        /html> <
+        /div>
+    );
 }
 
 export default App;
