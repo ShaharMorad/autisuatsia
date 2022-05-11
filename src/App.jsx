5715cf6cf6126387172cc89923207db44c37c82a
shaharmorad <shaharmorad13239@gmail.com>
2022-05-11 18:12:58 +0300

    situation page with video

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+import SituationPage from './components/SituationPage/SituationPage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SituationPage situationId={"asdasd"}/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
