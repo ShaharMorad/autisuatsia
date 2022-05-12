4772699417afeb43d1292f59b7f782cb3ea1f9f5
ivrifreud <ivrifreud@gmail.com>
2022-05-12 07:27:50 +0300

    final homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,45 @@
 import'./Home.css';
-import TextField from '@mui/material/TextField';
-import AppBar from '@mui/material/AppBar';
-import { Box } from '@mui/system';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 const Home = () => {
   return (
     <div class="messages">
-        <div class="about-us-section">
-          <p 
-          style={{
-            borderBottom: "1px solid", 
-            fontSize: "20px"
-          }}>
-             עלינו 
-          </p>
-          <p 
-          style={{
-            fontSize: "16px"
-          }}>באתר תוכלו למצוא תכנים המאפשרים לאוטיסטים ובעלי מוגבלויות לחוות סיטואציות נפוצות מחיי היומיום כמו ביקור במספרה, או קניות בסופרמרקט. 
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="טכנולוגיה שמקרבת"
+          height="200px"
+          image="\images\autism.jpg"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          עלינו
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          באתר תוכלו למצוא תכנים המאפשרים לאוטיסטים ובעלי מוגבלויות לחוות סיטואציות נפוצות מחיי היומיום כמו ביקור במספרה, או קניות בסופרמרקט. 
           התכנים מועברים באמצעות משקפי מציאות מדומה (VR), סרטון פנורמי (360) או דרך דיאלוגים המועברים בין בעזרת אדם המנחה את הדיאלוג.
-          </p>         
-        </div>
-        <div class="managers-section">
-          <p 
-          style={{
-            borderBottom: "1px solid", 
-            fontSize: "20px"
-          }}>לבעלי העסק מבינינו...</p>
-          <p 
-          style={{
-            fontSize: "16px"
-          }}>אתם מוזמנים לצלם את בית העסק שלכם ולצרף סיור וירטואלי שלו לתכני אתר וכך לאפשר לאוטיסטים ובעלי מוגבלויות לסייר בבית העסק שלכם טרם הביקור שלהם ולהפוך את השהות שלהם בבית העסק לקלה וחיובית יותר.</p> 
-        </div>
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="עזרה כפולה"
+          height="200px"
+          image="\images\owner.jpg"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          לבעלי העסק מבינינו...
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          אתם מוזמנים לצלם את בית העסק שלכם ולצרף סיור וירטואלי שלו לתכני האתר, ובכך לאפשר לאוטיסטים ובעלי מוגבלויות לסייר בבית העסק שלכם טרם הביקור שלהם, ולהפוך את השהות שלהם בבית העסק לקלה וחיובית יותר. 
+          </Typography>
+        </CardContent>
+      </Card>
         <div class="bottom-bar">
         <p 
           style={{
